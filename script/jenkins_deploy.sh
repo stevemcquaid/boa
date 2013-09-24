70edf03d80620e8917d89df8a2bdf7da09da10a8
Steve McQuaid <steve@stevemcquaid.com>
2013-09-24 17:01:22 -0400

    jenkins deploy script

---
+++ script/jenkins_deploy.sh
@@ -0,0 +1,25 @@
+#this may error if any configs are changed
+bundle install
+ls
+
+#this task will drop old data, create new db, run migratiosn and seed it
+rake db:populate 
+
+# #Testing
+# rake db:test:load
+
+rake ci:setup:testunit test
+#rake ci:setup:rspec spec
+#rake ci:setup:cucumber features
+# 
+# # Run all unit tests
+# rake test:units
+# # Run all functional tests
+# rake test:functionals
+# # Run all integration tests
+# rake test:integration
+
+
+cap development deploy
+
+echo 'Hello World It works! Successful build, test, and deploy!'
