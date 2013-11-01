143c4cd5615f8589c05ab5762220c31907bc92e5
Steve McQuaid <steve@stevemcquaid.com>
2013-11-01 14:27:43 -0400

    Updated environments

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :bundle_without, [:development, :test, :staging]
 set :rails_env, 'production'
-set :branch, :stage
-server 'trailer-d02.springcarnival.org', :app, :web, :db, :primary => true
+set :branch, :master
+server 'trailer-02.springcarnival.org', :app, :web, :db, :primary => true
 
 set :bundle_dir, ''
 set :bundle_flags, '--system --quiet'
