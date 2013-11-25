057075e405d995c3e432c761f8be19d7db9ffed3
Dylan Corwin <dylandcor@gmail.com>
2013-11-24 22:28:38 -0500

    auto focus for tool checkout

--- app/assets/javascripts/tool_checkin_checkout.js
+++ app/assets/javascripts/tool_checkin_checkout.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-    $("#new_checkout").submit(function(e) {
-        if($("#checkout_card_number").val() == "") {
-          $("#checkout_card_number").focus();
-          e.preventDefault();
-          return false;
-              
-        } else if($("#checkout_tool_id").val() == "") {
-          $("#checkout_tool_id").focus();
-          e.preventDefault();
-          return false;
-        }
-    });
+  var card_number_input = $("#checkout_card_number");
+  var tool_id_input = $("#checkout_tool_id");
+
+  if(tool_id_input.val() == "") {
+    tool_id_input.focus();
+  } else {
+    card_number_input.focus();
+  }
+
+  $("#new_checkout").submit(function(e) {
+      if(card_number_input.val() == "") {
+        card_number_input.focus();
+        e.preventDefault();
+        return false;
+            
+      } else if(tool_id_input.val() == "") {
+        tool_id_input.focus();
+        e.preventDefault();
+        return false;
+      }
+  });
 });
