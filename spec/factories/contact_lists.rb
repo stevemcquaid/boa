b6656558220b1d302dda0dd956c6c8280a26587e
Chase Brownell <minigeek@gmail.com>
2013-11-25 22:05:49 -0500

    Delete rpec tests and move factories to minitest and clean up factories

--- spec/factories/contact_lists.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :contact_list do
-    participant_id 1
-  end
-end
--- spec/models/contact_list_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe ContactList do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
