e2b9b4743a36a914d0e8cf162fe9fbdcd601980d
Steve McQuaid <steve@stevemcquaid.com>
2013-11-30 16:38:26 -0500

    oops syntax error - forgot end keyword

--- spec/features/organizations_spec.rb
+++ spec/features/organizations_spec.rb
@@ -6,18 +6,21 @@ describe "Organizations" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get organizations_path
       response.status.should be(200)
-describe "the signin process", :type => :feature do
-  before :each do
-    User.make(:email => 'smcquaid@andrew.cmu.edu', :password => 'testtest')
-  end
-
-  it "signs me in" do
-    visit '/sessions/new'
-    within("#session") do
-      fill_in 'Login', :with => 'smcquaid@andrew.cmu.edu'
-      fill_in 'Password', :with => 'testtest'
+  
+  describe "the signin process", :type => :feature do
+    before :each do
+      User.make(:email => 'smcquaid@andrew.cmu.edu', :password => 'testtest')
     end
-    click_link 'Sign in'
-    expect(page).to have_content 'Success'
+
+    it "signs me in" do
+      visit '/sessions/new'
+      within("#session") do
+        fill_in 'Login', :with => 'smcquaid@andrew.cmu.edu'
+        fill_in 'Password', :with => 'testtest'
+      end
+      click_link 'Sign in'
+      expect(page).to have_content 'Success'
+    end
   end
+  
 end
