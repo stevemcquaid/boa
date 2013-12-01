f1ca3ae24d02209d9e2a346672e683f0f3b555c9
Steve McQuaid <steve@stevemcquaid.com>
2013-12-01 18:05:54 -0500

    db stuff

--- script/jenkins_deploy.sh
+++ script/jenkins_deploy.sh
@@ -4,47 +4,11 @@
 
 #might be necessary?
 rm Gemfile.lock
-gem uninstall capistrano -v 3.0.0
-gem install capistrano -v 2.15.5
+#gem uninstall capistrano -v 3.0.0
+#gem install capistrano -v 2.15.5
 
-
-# echo 'configuring postgres db'
-# stagedb='boastage'
-# proddb='boaprod'
-# username='boauser'
-# psword='Prof_H_No_Sn00p1ng'
-psql << EOF
-
-DROP DATABASE devrv;
-DROP DATABASE jenkins;
-DROP DATABASE boauser;
-DROP DATABASE boatest;
-DROP DATABASE boadev;
-DROP DATABASE boastage;
-DROP DATABASE boaprod;
-
-DROP USER devsrv;
-CREATE USER devsrv WITH PASSWORD '1234';
-ALTER USER devsrv WITH SUPERUSER;
-
-DROP USER jenkins;
-CREATE USER jenkins WITH PASSWORD '1234';
-ALTER USER jenkins WITH jenkins;
- 
-DROP USER boauser;
-CREATE USER boauser WITH PASSWORD 'Prof_H_No_Sn00p1ng';
-ALTER USER boauser WITH SUPERUSER;
-
-CREATE DATABASE jenkins OWNER jenkins;
-CREATE DATABASE devsrv OWNER devsrv;
-CREATE DATABASE boauser OWNER boauser;
-CREATE DATABASE boatest OWNER boauser;
-CREATE DATABASE boadev OWNER boauser;
-CREATE DATABASE boastage OWNER boauser;
-CREATE DATABASE boaprod OWNER boauser;
- 
-EOF
-
+#run db script
+. $PWD/script/db.sh
 
 bundle install
 ls
