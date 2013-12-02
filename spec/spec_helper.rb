695a97a6a3eee6877f28e589f6f235df7d1bc1be
Steve McQuaid <steve@stevemcquaid.com>
2013-12-01 20:16:49 -0500

    rake ci:setup:rspec spec:features on boa/jenkins shoudl work now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'email_spec'
+require 'capybara/rspec'
 require 'capybara/rails'
 # require Devise::TestHelpers
 
