3d60cf58e01323d8d1ea0af499ffc7f70d7f540e
Dylan Corwin <dylandcor@gmail.com>
2013-12-02 15:07:00 -0500

    added more unit tests and changed document preview size

--- test/unit/participant_test.rb
+++ test/unit/participant_test.rb
@@ -30,6 +30,13 @@ class ParticipantTest < ActiveSupport::TestCase
       should "know participants" do
         assert_equal ["asteger_FG", "dcorwin_FG", "juc", "member_FG", "rcrown_FG", "shannon1_FG"], Participant.all.map{|e| e.andrewid}
       end
+
+      should "correctly format a participant's phone number" do
+        assert_equal "(123) 456-7890", @member_participant.formatted_phone_number
+
+        @temp_participant = FactoryGirl.build(:participant, :phone_number => nil)
+        assert_equal "N/A", @temp_participant.formatted_phone_number
+      end
     end
     
   end
